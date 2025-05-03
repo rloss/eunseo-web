700a225781373bf5ea66f2fe40360bae5758fdee
rloss <chnnsx@gmail.com>
2025-05-03 13:50:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render 환경이 제공하는 포트 사용
+    app.run(host="0.0.0.0", port=port)
