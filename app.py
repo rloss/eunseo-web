053836496322dac27fab9069992e6c5e1a9a8fb2
rloss <rlossqianss@gmail.com>
2025-05-04 10:21:58 +0900

    � 초기 커밋: 은서 실험실 구조

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
-
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route("/log")
+def log():
+    return render_template("log.html")
+
+@app.route("/lab")
+def lab():
+    return render_template("lab.html")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000)) 
+    app.run(host="0.0.0.0", port=port)
